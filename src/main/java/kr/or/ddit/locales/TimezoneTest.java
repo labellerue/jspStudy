d6b30639baf92e6202eb300e87f57325df282b41
labellerue <so4741@gmail.com>
2018-10-29 15:24:54 +0900

    junitest 주석처리

--- src/main/java/kr/or/ddit/locales/TimezoneTest.java
+++ src/main/java/kr/or/ddit/locales/TimezoneTest.java
@@ -3,22 +3,22 @@ package kr.or.ddit.locales;
 
 //import java.util.TimeZone;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 public class TimezoneTest {
 
-	@Test
-	public void timezoneTest() {
-		/*** Given ***/
-		
-		/*** When ***/
-		//시간 timezone 정보 제공되는 나라 
+//	@Test
+//	public void timezoneTest() {
+//		/*** Given ***/
+//		
+//		/*** When ***/
+//		시간 timezone 정보 제공되는 나라 
 //		String[] timezones = TimeZone.getAvailableIDs();
 //		for(String str : timezones){
 //			System.out.println(str);
 //		}
-		/*** Then ***/
-
-	}
+//		/*** Then ***/
+//
+//	}
 
 }
